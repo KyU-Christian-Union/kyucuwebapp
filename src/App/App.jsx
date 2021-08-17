3de4e2f2112775bcb630c189a45197fe39bde32e
dijiflex <dijiflex@gmail.com>
2021-08-17 14:57:36 +0300

    Imported the homepage to  App.js

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import HomePage from '../pages/HomePage/HomePage';
 
 function App() {
   return (
-    <div>
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
-    </div>
+    <>
+      <HomePage />
+    </>
   );
 }
 
