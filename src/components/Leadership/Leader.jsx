a6b6e753f5a8d66c458bda274b2f31aba1270754
Luke Reign <lkibiku@gmail.com>
2021-09-01 09:08:39 +0300

    Add dotted steppers

--- src/components/Leadership/Leader.jsx
+++ src/components/Leadership/Leader.jsx
@@ -1,3 +1,70 @@
+// import React from 'react';
+// import { makeStyles, useTheme } from '@material-ui/core/styles';
+// import MobileStepper from '@material-ui/core/MobileStepper';
+// import Paper from '@material-ui/core/Paper';
+// import Typography from '@material-ui/core/Typography';
+// import Button from '@material-ui/core/Button';
+// import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+// import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+
+// const tutorialSteps = [
+//   {
+//     label: 'San Francisco – Oakland Bay Bridge, United States',
+//     imgPath: 'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60'
+//   },
+//   {
+//     label: 'Bird',
+//     imgPath: 'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60'
+//   },
+//   {
+//     label: 'Bali, Indonesia',
+//     imgPath: 'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80'
+//   },
+//   {
+//     label: 'NeONBRAND Digital Marketing, Las Vegas, United States',
+//     imgPath: 'https://images.unsplash.com/photo-1518732714860-b62714ce0c59?auto=format&fit=crop&w=400&h=250&q=60'
+//   },
+//   {
+//     label: 'Goč, Serbia',
+//     imgPath: 'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60'
+//   }
+// ];
+
+// const useStyles = makeStyles(theme => ({
+//   root: {
+//     maxWidth: 400,
+//     flexGrow: 1
+//   },
+//   header: {
+//     display: 'flex',
+//     alignItems: 'center',
+//     height: 50,
+//     paddingLeft: theme.spacing(4),
+//     backgroundColor: theme.palette.background.default
+//   },
+//   img: {
+//     height: 255,
+//     display: 'block',
+//     maxWidth: 400,
+//     overflow: 'hidden',
+//     width: '100%'
+//   }
+// }));
+
+// function SwipeableTextMobileStepper() {
+//   const classes = useStyles();
+//   const theme = useTheme();
+//   const [activeStep, setActiveStep] = React.useState(0);
+//   const maxSteps = tutorialSteps.length;
+
+//   const handleNext = () => {
+//     setActiveStep(prevActiveStep => prevActiveStep + 1);
+//   };
+
+//   const handleBack = () => {
+//     setActiveStep(prevActiveStep => prevActiveStep - 1);
+//   };
+
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -40,83 +107,3 @@ const Leader = () => {
 };
 
 export default Leader;
-
-// import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
-// import Card from '@material-ui/core/Card';
-// import CardActionArea from '@material-ui/core/CardActionArea';
-// import CardContent from '@material-ui/core/CardContent';
-// import CardMedia from '@material-ui/core/CardMedia';
-// import Typography from '@material-ui/core/Typography';
-// import leaderImage from '../../assets/images/leader1.png';
-
-// const useStyles = makeStyles({
-//   root: {
-//     maxWidth: 345
-//   }
-// });
-
-// export default function ImgMediaCard() {
-//   const classes = useStyles();
-
-//   return (
-//     <Card className={classes.root}>
-//       <CardActionArea>
-//         <CardMedia
-//           component="img"
-//           alt="Chairperson"
-//           height="140"
-//           image="../../assets/images/leader1.png"
-//           title="Amos Koine"
-//         />
-//         <CardContent>
-//           <Typography gutterBottom variant="h5" component="h2">
-//             Amos Koine
-//           </Typography>
-//           <Typography variant="body2" color="textSecondary" component="p">
-//             Chairperson
-//           </Typography>
-//         </CardContent>
-//       </CardActionArea>
-//     </Card>
-//   );
-// }
-
-// // import React from 'react';
-// // import { makeStyles } from '@material-ui/core/styles';
-// // import ImageList from '@material-ui/core/ImageList';
-// // import ImageListItem from '@material-ui/core/ImageListItem';
-// // import ImageListItemBar from '@material-ui/core/ImageListItemBar';
-// // import itemData from './itemData';
-
-// // const useStyles = makeStyles({
-// //   root: {
-// //     display: 'flex',
-// //     flexWrap: 'wrap',
-// //     justifyContent: 'space-evenly',
-// //     alignContent: 'center',
-// //     overflow: 'hidden'
-// //   },
-// //   imageList: {
-// //     width: 500,
-// //     height: 450
-// //   }
-// // });
-
-// // export default function TitlebarImageList() {
-// //   const classes = useStyles();
-
-// //   return (
-// //     <div className={classes.root}>
-// //       <ImageList rowHeight={180} className={classes.imageList}>
-// //         <ImageListItem style={{ width: 'auto' }} />
-// //         {itemData.map(item => (
-// //           <ImageListItem key={item.img}>
-// //             <img src={item.img} alt={item.name} />
-// //             <ImageListItemBar title={item.name} subtitle={<span> {item.position}</span>} />
-// //           </ImageListItem>
-// //         ))}
-// //       </ImageList>
-// //     </div>
-// //   );
-// // }
--- src/components/Leadership/Leadership.jsx
+++ src/components/Leadership/Leadership.jsx
@@ -2,16 +2,31 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import Leader from './Leader';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    marginTop: theme.spacing(3)
+    marginTop: theme.spacing(3),
+    flexGrow: 1
   }
 }));
 const Leadership = () => {
   const classes = useStyles();
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep(prevActiveStep => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep(prevActiveStep => prevActiveStep - 1);
+  };
   return (
     <div>
       <Typography variant="h1" className={classes.heading} align="center">
@@ -33,6 +48,25 @@ const Leadership = () => {
           </Grid>
         </Grid>
       </Container>
+      <MobileStepper
+        variant="dots"
+        steps={4}
+        position="static"
+        activeStep={activeStep}
+        className={classes.root}
+        nextButton={
+          <Button size="small" onClick={handleNext} disabled={activeStep === 3}>
+            Next
+            {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+            Back
+          </Button>
+        }
+      />
     </div>
   );
 };
