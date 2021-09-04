f97adebcc629bff21eec96121907feac4cda98ca
Luke Reign <lkibiku@gmail.com>
2021-09-04 19:52:50 +0300

    Add container to the leader component

--- src/components/Leadership/Leader.jsx
+++ src/components/Leadership/Leader.jsx
@@ -1,70 +1,3 @@
-// import React from 'react';
-// import { makeStyles, useTheme } from '@material-ui/core/styles';
-// import MobileStepper from '@material-ui/core/MobileStepper';
-// import Paper from '@material-ui/core/Paper';
-// import Typography from '@material-ui/core/Typography';
-// import Button from '@material-ui/core/Button';
-// import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-// import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
-
-// const tutorialSteps = [
-//   {
-//     label: 'San Francisco – Oakland Bay Bridge, United States',
-//     imgPath: 'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60'
-//   },
-//   {
-//     label: 'Bird',
-//     imgPath: 'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60'
-//   },
-//   {
-//     label: 'Bali, Indonesia',
-//     imgPath: 'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80'
-//   },
-//   {
-//     label: 'NeONBRAND Digital Marketing, Las Vegas, United States',
-//     imgPath: 'https://images.unsplash.com/photo-1518732714860-b62714ce0c59?auto=format&fit=crop&w=400&h=250&q=60'
-//   },
-//   {
-//     label: 'Goč, Serbia',
-//     imgPath: 'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60'
-//   }
-// ];
-
-// const useStyles = makeStyles(theme => ({
-//   root: {
-//     maxWidth: 400,
-//     flexGrow: 1
-//   },
-//   header: {
-//     display: 'flex',
-//     alignItems: 'center',
-//     height: 50,
-//     paddingLeft: theme.spacing(4),
-//     backgroundColor: theme.palette.background.default
-//   },
-//   img: {
-//     height: 255,
-//     display: 'block',
-//     maxWidth: 400,
-//     overflow: 'hidden',
-//     width: '100%'
-//   }
-// }));
-
-// function SwipeableTextMobileStepper() {
-//   const classes = useStyles();
-//   const theme = useTheme();
-//   const [activeStep, setActiveStep] = React.useState(0);
-//   const maxSteps = tutorialSteps.length;
-
-//   const handleNext = () => {
-//     setActiveStep(prevActiveStep => prevActiveStep + 1);
-//   };
-
-//   const handleBack = () => {
-//     setActiveStep(prevActiveStep => prevActiveStep - 1);
-//   };
-
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -76,19 +9,31 @@ import leaderImage from '../../assets/images/leader1.png';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    maxWidth: 345
+    maxWidth: 345,
+    margin: 10,
+    backgroundColor: '#EDEDED'
+  },
+  media: {
+    backgroundPosition: 'center',
+    backgroundSize: 'contain',
+    backgroundRepeat: 'no-repeat',
+    height: '100%',
+    width: '100%'
   },
   leaderheader: {
     h4: theme.typography.h4,
-    fontWeight: '900',
-    fontSize: '15'
+    fontWeight: '900'
+  },
+  leadersubheader: {
+    h5: theme.typography.h5,
+    fontWeight: '400'
   }
 }));
+
 const Leader = () => {
   const classes = useStyles();
-
   return (
-    <Card className={classes.root}>
+    <Card className={classes.root} m={2}>
       <CardActionArea>
         <CardMedia height="140">
           <img className={classes.media} src={leaderImage} alt="leaderImage" />
@@ -97,7 +42,7 @@ const Leader = () => {
           <Typography variant="h4" align="center" className={classes.leaderheader}>
             Amos Koine
           </Typography>
-          <Typography variant="body1" align="center">
+          <Typography variant="h5" align="center" className={classes.leadersubheader}>
             Chairperson
           </Typography>
         </CardContent>
--- src/components/Leadership/Leadership.jsx
+++ src/components/Leadership/Leadership.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import { Box, Button, Container, Grid } from '@material-ui/core';
 import MobileStepper from '@material-ui/core/MobileStepper';
-import Button from '@material-ui/core/Button';
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import Leader from './Leader';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    marginTop: theme.spacing(3),
-    flexGrow: 1
+    backgroundColor: '#EDEDED',
+    width: 'auto'
+  },
+  heading: {
+    h1: theme.typography.h1,
+    fontWeight: '600',
+    [theme.breakpoints.down('xs')]: {
+      fontSize: '2.5rem',
+      paddingTop: theme.spacing(0.8),
+      paddingBottom: theme.spacing(0.8)
+    },
+    [theme.breakpoints.up('sm')]: {
+      fontSize: '3.5rem'
+    }
+  },
+  button: {
+    margin: 15,
+    fontSize: 15
   }
 }));
+
 const Leadership = () => {
   const classes = useStyles();
   const theme = useTheme();
@@ -28,46 +43,60 @@ const Leadership = () => {
     setActiveStep(prevActiveStep => prevActiveStep - 1);
   };
   return (
-    <div>
+    <Container maxWidth="lg" className={classes.root}>
       <Typography variant="h1" className={classes.heading} align="center">
-        Our Leadership
+        Our Leaders
       </Typography>
-      <Container maxWidth="lg" className={classes.root}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={6} md={3} lg={3}>
+      <Container>
+        <Grid container spacing={2} justifyContent="center">
+          <Grid item xs={12} sm={6} md={4} lg={3}>
             <Leader />
           </Grid>
-          <Grid item xs={12} sm={6} md={3} lg={3}>
+          <Grid item xs={12} sm={6} md={4} lg={3}>
             <Leader />
           </Grid>
-          <Grid item xs={12} sm={6} md={3} lg={3}>
+          <Grid item xs={12} sm={6} md={4} lg={3}>
             <Leader />
           </Grid>
-          <Grid item xs={12} sm={6} md={3} lg={3}>
+          <Grid item xs={12} sm={6} md={4} lg={3}>
             <Leader />
           </Grid>
         </Grid>
       </Container>
-      <MobileStepper
-        variant="dots"
-        steps={4}
-        position="static"
-        activeStep={activeStep}
-        className={classes.root}
-        nextButton={
-          <Button size="small" onClick={handleNext} disabled={activeStep === 3}>
-            Next
-            {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-          </Button>
-        }
-        backButton={
-          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-            Back
-          </Button>
-        }
-      />
-    </div>
+      <Box textAlign="center">
+        <MobileStepper
+          variant="dots"
+          steps={4}
+          position="static"
+          activeStep={activeStep}
+          className={classes.root}
+          nextButton={
+            <Button
+              size="small"
+              onClick={handleNext}
+              disabled={activeStep === 3}
+              className={classes.button}
+              color="primary"
+            >
+              Next
+              {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+            </Button>
+          }
+          backButton={
+            <Button
+              size="small"
+              onClick={handleBack}
+              disabled={activeStep === 0}
+              className={classes.button}
+              color="primary"
+            >
+              {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+              Back
+            </Button>
+          }
+        />
+      </Box>
+    </Container>
   );
 };
 
