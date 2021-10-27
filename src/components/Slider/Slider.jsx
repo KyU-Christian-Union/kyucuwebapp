afb5808581ec87c8013b77562008f73a7043da9c
unknown <kimitawanjohi7923@gmail.com>
2021-10-27 12:18:40 +0300

    fixed wrap resposiveness

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import './slider.css';
 import { Carousel } from 'react-responsive-carousel';
-import upcomingeventImage from '../../assets/images/upcomingevent_image.jpg';
 
 // prettier-ignore
 const data = [
@@ -15,7 +15,7 @@ const data = [
       },
       {
           id: 2,
-        image:'`https://wallpapercave.com/wp/wp9901724.jpg',
+        image:'https://wallpapercave.com/wp/wp9901837.jpg',
         title: 'Welcome To Kirinyaga University Christian Union',
         text: 'Serving God with fervent Worship'
       },
@@ -30,18 +30,21 @@ const data = [
 // prettier-ignore
 export default function Slider() {
     return (
+        <Container disableGutters maxWidth='xl' >
             <Carousel showThumbs={false} >
             { data.map(slide => (
                 <div key={ slide.id }>
-                    <img src={ upcomingeventImage } alt='slide' className="slide-image" />
+                    <img src={ slide.image } alt='slide' className="slide-image" />
                     <Typography 
                      variant='h1' 
                      component='p'
                      className='slider-title'
-                    > { slide.title } <br /> <Typography variant="h2" component="p"> { slide.text} </Typography>
+                     noWrap
+                    > { slide.title } <br /> <Typography variant="h2" noWrap  component="p"> { slide.text} </Typography>
                     </Typography>
                 </div>
             ))}
             </Carousel>
+        </Container>
     );
 }
