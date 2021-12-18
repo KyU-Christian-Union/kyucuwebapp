4c45b80474988b920ed22b1ac5e529a626b7a06f
otwoma <otwomadennis365@gmail.com>
2021-12-18 14:43:04 +0300

    finished up the testimonials part, all the testimonials card well designed and everything is working

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,18 +1,36 @@
-import { Container } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import React from 'react';
 import { makeStyles } from '@mui/styles';
 import Carousel from 'react-elastic-carousel';
 import Testimonial from './Testimonial';
+import image1 from '../../assets/images/user-1.jpg';
+import image2 from '../../assets/images/user2.jpg';
+import image3 from '../../assets/images/user3.jpg';
+import image4 from '../../assets/images/user4.jpg';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    marginTop: theme.spacing(2),
+    marginTop: theme.spacing(5),
     marginBottom: theme.spacing(2)
   },
   testimonials_container: {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  title: {
+    fontSize: '48px',
+    lineHeight: '60px',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: '10px',
+    color: theme.palette.primary.dark
+  },
+  testimonials_intro_text: {
+    textAlign: 'center',
+    lineHeight: '30px',
+    marginBottom: '10px',
+    color: theme.palette.primary.light
   }
 }));
 
@@ -27,10 +45,53 @@ const Testimonials = () => {
   const classes = useStyles();
   return (
     <Container className={classes.root}>
+      <Typography className={classes.title}>Testimonials</Typography>
+      <Typography className={classes.testimonials_intro_text}>
+        Get to know what the Lord has done and is doing in people&apos;s lives
+      </Typography>
       <Carousel breakPoints={breakPoints}>
-        <Testimonial dummyText="this is a sample testimonial card we are showing so this is the first" />
-        <Testimonial dummyText="this is a sample testimonial card we are showing so this is the second" />
-        <Testimonial dummyText="this is a sample testimonial card we are showing  so this is the third" />
+        <Testimonial
+          commentTitle="amazing leadership"
+          comment="sometimes in our lives, we all have pain and sorrow, but if we are wise, we know that there is always tomorrow and that is why I decided to become part of the christian union Kirinyaga university"
+          commentorProfilePicture={image1}
+          commentorName="Kevin Maunga"
+          commentorPosition="fourth year student"
+        />
+        <Testimonial
+          commentTitle="supportive community"
+          comment="sometimes in our lives, we all have pain and sorrow, but if we are wise, we know that there is always tomorrow and that is why I decided to become part of the christian union Kirinyaga university"
+          commentorProfilePicture={image2}
+          commentorName="Jack sylvester"
+          commentorPosition="medicine graduate, JKUAT"
+        />
+        <Testimonial
+          commentTitle="organized order of service"
+          comment="sometimes in our lives, we all have pain and sorrow, but if we are wise, we know that there is always tomorrow and that is why I decided to become part of the christian union Kirinyaga university"
+          commentorProfilePicture={image4}
+          commentorName="Ann Wangari"
+          commentorPosition="fourth year student"
+        />
+        <Testimonial
+          commentTitle="nice ushers"
+          comment="sometimes in our lives, we all have pain and sorrow, but if we are wise, we know that there is always tomorrow and that is why I decided to become part of the christian union Kirinyaga university"
+          commentorProfilePicture={image3}
+          commentorName="claire anita"
+          commentorPosition="lecture, University of Nairobi"
+        />
+        <Testimonial
+          commentTitle="nice ushers"
+          comment="sometimes in our lives, we all have pain and sorrow, but if we are wise, we know that there is always tomorrow and that is why I decided to become part of the christian union Kirinyaga university"
+          commentorProfilePicture={image3}
+          commentorName="claire anita"
+          commentorPosition="lecture, University of Nairobi"
+        />
+        <Testimonial
+          commentTitle="nice ushers"
+          comment="sometimes in our lives, we all have pain and sorrow, but if we are wise, we know that there is always tomorrow and that is why I decided to become part of the christian union Kirinyaga university"
+          commentorProfilePicture={image3}
+          commentorName="claire anita"
+          commentorPosition="lecture, University of Nairobi"
+        />
       </Carousel>
     </Container>
   );
