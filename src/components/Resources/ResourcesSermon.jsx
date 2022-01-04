f3de1ad62173c7fe37bd9c8fd18fada7e104e56f
ANNMAINAWANGARI <wangariann371@gmail.com>
2022-01-04 14:27:49 +0300

    resource sermon frontend code change

--- src/components/Resources/ResourcesSermon.jsx
+++ src/components/Resources/ResourcesSermon.jsx
@@ -1,57 +1,57 @@
-import { Grid, Typography, Box } from '@mui/material';
+import { Typography, Box, Card, CardActions, CardContent } from '@mui/material';
 import React from 'react';
 import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
 import ShareIcon from '@mui/icons-material/Share';
 import YouTubeIcon from '@mui/icons-material/YouTube';
+import makeStyles from '@mui/styles/makeStyles';
+import Stack from '@mui/material/Stack';
 import SermonImage from '../../assets/images/sermon_image.jpg';
 
+const useStyles = makeStyles(() => ({
+  Card: {
+    backgroundImage: `url(${SermonImage})`,
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover'
+  }
+}));
+
 const ResourcesSermon = () => {
+  const classes = useStyles();
   return (
-    <Grid item p={2} direction="column" sx={{ color: 'white' }}>
-      <Grid
-        container
-        sx={{
-          backgroundImage: `url(${SermonImage})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center'
-        }}
-      >
-        {/** name and icons */}
-        <Grid container mb={8} p={2} justifyContent="space-between">
-          <Typography variant="h5">Lorem ipsum dolor sit</Typography>
-          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Grid mr={2}>
-              <AccessTimeFilledIcon />
-              <Typography>watch</Typography>
-            </Grid>
-            <Grid>
-              <ShareIcon />
-              <Typography>share</Typography>
-            </Grid>
-          </Box>
-        </Grid>
-        {/** youtube icon */}
-        <Grid container mb={8} justifyContent="center">
-          <YouTubeIcon fontSize="large" />
-        </Grid>
-        {/** watch on youtube */}
-        <Grid container pl={2} mb={2} alignItems="center">
-          <Typography variant="h6">Watch on</Typography>
-          <YouTubeIcon />
-          <Typography variant="h6">Youtube</Typography>
-        </Grid>
-      </Grid>
-      <Grid container mt={2} sx={{ color: 'black' }}>
-        <Typography gutterBottom variant="h3" sx={{ fontWeight: 800 }}>
+    <Card>
+      <CardActions className={classes.Card} sx={{ display: 'flex', flexDirection: 'column', height: '350px' }}>
+        <Stack direction="row" spacing={5} sx={{ minWidth: '100%' }}>
+          <Stack direction="row" spacing={1}>
+            <YouTubeIcon sx={{ color: 'red' }} />
+            <Typography variant="h6" color="white">
+              KyU CU Sermon
+            </Typography>
+          </Stack>
+          <Stack direction="row">
+            <Stack>
+              <AccessTimeFilledIcon sx={{ color: 'white' }} />
+              <Typography color="white">Watch Later</Typography>
+            </Stack>
+            <Stack>
+              <ShareIcon sx={{ color: 'white' }} />
+              <Typography color="white">Share</Typography>
+            </Stack>
+          </Stack>
+        </Stack>
+        <Box mt={15} sx={{ alignItems: 'center', justifyContent: 'center' }}>
+          <YouTubeIcon sx={{ color: 'red' }} fontSize="large" />
+        </Box>
+      </CardActions>
+      <CardContent>
+        <Typography variant="h4" gutterBottom color="#2E002E">
           Lorem ipsum dolor sit
         </Typography>
-      </Grid>
-      <Grid container sx={{ color: 'black' }}>
-        <Typography variant="h5" sx={{ fontWeight: 400 }}>
-          Lorem ipsum dolor sit amet, consectetur
+        <Typography variant="h6" color="#008000">
+          Lorem ipsum dolor sit
         </Typography>
-      </Grid>
-    </Grid>
+      </CardContent>
+    </Card>
   );
 };
 
