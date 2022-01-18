ac28aa84a7177661208dc619d1b8999c383915fb
ANNMAINAWANGARI <wangariann371@gmail.com>
2022-01-18 18:17:06 +0300

    give page typography styling complete

--- src/pages/GivePage/GivePage.jsx
+++ src/pages/GivePage/GivePage.jsx
@@ -1,4 +1,4 @@
-import { Container } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import React from 'react';
 import PublicAppBar from '../../components/AppBars.jsx/PublicAppBar';
 import Banner from '../../components/Banner/Banner';
@@ -9,7 +9,25 @@ const GivePage = () => {
     <>
       <PublicAppBar />
       <Banner imgSource={BannerImg} heading="Give" subtitle="Your Generosity Is Making A Difference" />
-      <Container>givegive</Container>
+      <Container maxWidth="lg">
+        <Typography variant="h6" align="center" gutterBottom sx={{ paddingTop: 3 }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim nunc pretium risus sit scelerisque non viverra.
+          Luctus nulla vitae sodales ullamcorper urna. Posuere ut platea etiam molestie vulputate purus, placerat in
+          aliquet. Cras lorem sagittis, massa pharetra adipiscing eu, consectetur mattis. Elementum vel nunc tincidunt
+          aenean varius. Laoreet elementum eget feugiat aliquam arcu scelerisqueLorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Enim nunc pretium risus sit scelerisque non viverra. Luctus nulla vitae sodales ullamcorper
+          urna. Posuere ut platea etiam molestie vulputate purus, placerat in aliquet. Cras lorem sagittis, massa
+          pharetra adipiscing eu, consectetur mattis. Elementum vel nunc tincidunt aenean varius. Laoreet elementum eget
+          feugiat aliquam arcu scelerisque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim nunc pretium
+          risus sit scelerisque non viverra. Luctus nulla vitae sodales ullamcorper urna. Posuere ut platea etiam
+          molestie vulputate purus, placerat in aliquet. Cras lorem sagittis, massa pharetra adipiscing eu, consectetur
+          mattis. Elementum vel nunc tincidunt aenean varius. Laoreet elementum eget feugiat aliquam arcu
+          scelerisqueLorem ipsum dolor sit amet, consectetur adipiscing elit. Enim nunc pretium risus sit scelerisque
+          non viverra. Luctus nulla vitae sodales ullamcorper urna. Posuere ut platea etiam molestie vulputate purus,
+          placerat in aliquet. Cras lorem sagittis, massa pharetra adipiscing eu, consectetur mattis. Elementum vel nunc
+          tincidunt aenean varius. Laoreet elementum eget feugiat aliquam arcu scelerisque.
+        </Typography>
+      </Container>
     </>
   );
 };
